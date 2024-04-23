3e5001ebea27aa13871a0ac5e7fcf4446abc967b
akshay41 <akshujoshi41@gmail.com>
2024-04-23 08:53:42 +0100

    adding loadbalancer config

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-import express  from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path'; // Import path module
 
 dotenv.config();
 const app = express();
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname); // Get current directory name
+
 const PORT = process.env.PORT || 5000;
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res)=> res.send('hello world'));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
-app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
